123e736fa7eaa1ff29d01969964cf98e460c5516
Khanh Vinh <124950174+justmeomeo@users.noreply.github.com>
2024-04-11 13:22:16 +0700

    Giao vien khong day lop hoc trong quy 1 nam 2024
    Giao vien khong day lop hoc trong quy 1 nam 2024

--- NOT_IN-LEFT_RIGHT_JOIN/6.sql
+++ NOT_IN-LEFT_RIGHT_JOIN/6.sql
@@ -9,7 +9,5 @@ WHERE
         JOIN CLASS b ON a.TEACHER_ID = b.TEACHER_ID
         JOIN CLASS_DETAIL c ON b.CLASS_ID = c.CLASS_ID 
         WHERE
-            YEAR(c.DATE_START) = 2024
-            AND YEAR(c.DATE_END) = 2024 
-            AND MONTH(DATE_START) BETWEEN 1 and 3 
-            AND MONTH(DATE_END) BETWEEN 1 and 3);
+            YEAR(c.DATE_START) = 2024 
+            AND MONTH(DATE_START) IN 1 and 3;
