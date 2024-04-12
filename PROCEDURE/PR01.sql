bdf249ede8d20adf44875dd9f98bec85342c0f93
koha11 <110100100+koha11@users.noreply.github.com>
2024-04-12 16:02:38 +0700

    Create PR01.sql
    Tạo Tài khoản đăng nhập cho giáo viên hoặc học sinh

---
+++ PROCEDURE/PR01.sql
@@ -0,0 +1,13 @@
+CREATE PROC PR_CREATE_LOGIN 
+@UID VARCHAR(10), @PWD VARCHAR(20)
+AS BEGIN
+	DECLARE @SQL NVARCHAR(MAX)
+	SET @PWD = REPLACE(@PWD,'''', '''''')	
+	SET @SQL = 'use master; CREATE LOGIN ' + @UID +' WITH PASSWORD = '''  + @PWD + ''';' + 
+				'use QLTTTA; CREATE USER ' + @UID + ' FOR LOGIN ' + @UID; 
+	IF (@UID LIKE 'ST%')
+		SET @SQL = @SQL + ' ALTER ROLE student_role ADD MEMBER ' + @UID
+	ELSE 
+		SET @SQL = @SQL + ' ALTER ROLE teacher ADD MEMBER ' + @UID
+	exec(@SQL)
+END
